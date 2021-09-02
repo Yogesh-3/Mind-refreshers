ac00d373a87b0d8d5ab78cb9aee1076a59e7cce2
Yogesh R <62652369+Yogesh-3@users.noreply.github.com>
2021-09-02 10:42:09 +0530

    Add files via upload

---
+++ Maxium of three num.py
@@ -0,0 +1,7 @@
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
+num3 = int(input("3rd number: "))
+ 
+largest = max(num1, num2, num3)  # can also use if else statement
+ 
+print("Largest number you entered is: ",largest)
